95db156692986500e2d7d80d47e36775dd1d277e
Karan Bhoir <karan.b@sankeysolutions.com>
2020-12-19 20:48:22 +0530

    added padding for top inset in safeArea

--- src/CountryPicker.js
+++ src/CountryPicker.js
@@ -474,7 +474,7 @@ export default class CountryPicker extends Component {
         >
           <SafeAreaView 
             style={styles.modalContainer} 
-            forceInset={{ bottom: 'never' }}
+            forceInset={{ bottom: 'never', top: 44 }}
           >
             <View style={styles.header}>
               {this.props.closeable && (
